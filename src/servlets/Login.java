f6ca7fa92bc2bae4e28c50ef6d8d72298b10875f
Juan Pablo <Juan Pablo@Juanpablo2>
2020-05-20 13:57:32 +0200

    servet login

--- src/servlets/Login.java
+++ src/servlets/Login.java
@@ -8,6 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Usuario;
+import model.controladores.UsuarioControlador;
+
 /**
  * Servlet implementation class Login
  */
@@ -24,19 +30,21 @@ public class Login extends HttpServlet {
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode rootNode = mapper.readTree(request.getInputStream());
+		String userOrEmail = rootNode.path("usuarioOMail").asText();
+		String password = rootNode.path("pass").asText();
+		
+		System.out.println("Usuario: " + userOrEmail + " - Password: " + password);
+		
+		Usuario u = UsuarioControlador.getControlador().find(1);
+		
+		// Creo el JSON de salida y lo devuelvo al cliente
+		response.getWriter().println(mapper.writeValueAsString(u));
 	}
 
 }
